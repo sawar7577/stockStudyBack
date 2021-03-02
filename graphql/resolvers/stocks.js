eef8e817c2947b269b7f9558f3d84640a257b1ee
jagadale_commits <jagadale.new@gmail.com>
2021-03-02 22:34:15 +0530

    new site

--- graphql/resolvers/stocks.js
+++ graphql/resolvers/stocks.js
@@ -26,18 +26,11 @@ module.exports = {
     }
   },
   Mutation: {
-<<<<<<< HEAD
     async createStock(_, { ticker, prob}, context) {
       const user = checkAuth(context);
       var price = [];
       var prediction = new Array(100).fill(0);
       var predictedPrice = new Array(100).fill(0);
-=======
-    async createStock(_, { ticker }, context) {
-      const user = checkAuth(context);
-      var price = [];
-      var prediction = new Array(100).fill(0);
->>>>>>> 0d981e191552715386a538164330bbe346877203
       var decisions = new Array(100).fill(0);
       var timeStamp = new Array(100).fill(0);
       var cprediction = new Array(100).fill(0);
@@ -62,7 +55,6 @@ module.exports = {
         for(var i=0; i<99; i++)
         { 
           let va = Math.round(price[i]*10000);
-<<<<<<< HEAD
           price[i]= va/10;
         }
         var p = prob === "80%" ? 0.8 : 0.7;
@@ -96,38 +88,11 @@ module.exports = {
               predictedPrice[i] =  price[i]-(price[i+1]-price[i])/2+(price[i+1]-price[i])*Math.random();
             }
           }
-=======
-          price[i]= va/100;
-        }
-        var p =  Math.floor(Math.random()* 11 ) + 0;
-        p = 6;
-        var r = new Array(100).fill(0);
-        for(var i=0; i<p; i++)
-        {
-            let n = Math.floor(Math.random()*(80 - 49)) + 49;
-            while(r[n]!=0)
-            {
-              n = Math.floor(Math.random()*(80 - 49)) + 49;
-            }
-            r[n]=1;
-        }
-        for(var i=0; i<99; i++)
-        { 
-          prediction[i]= price[i+1]>=price[i] ? 1:0;
-        }
-        for(var i=49; i<=80; i++)
-        { 
-          if(r[i]==1)
-          prediction[i]=  prediction[i]==0 ? 1:0;
->>>>>>> 0d981e191552715386a538164330bbe346877203
         }
         
             const newStock = new Stock({
               closingPrice: price,
-<<<<<<< HEAD
               predictedPrice: predictedPrice,
-=======
->>>>>>> 0d981e191552715386a538164330bbe346877203
               prediction: prediction,
               decisions: decisions,
               timeStamp : timeStamp,
